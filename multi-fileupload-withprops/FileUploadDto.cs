2615a5a265f8c27a1f273a1f9a7102425b18a137
Sami <Al-Khatib.Sami@fronius.com>
2018-09-28 09:54:34 +0200

    initial commit

---
+++ multi-fileupload-withprops/FileUploadDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+
+namespace MultiFileuploadWithProps
+{
+    public class FileUploadDto
+    {
+        public IEnumerable<IFormFile> Files { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ multi-fileupload-withprops/FilesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiFileuploadWithProps
+{
+    [Route("api/[controller]")]
+    public class FilesController : Controller
+    {
+        [HttpPost("{id:int}")]
+        public IActionResult FileUpload([FromRoute] int id, [FromForm] FileUploadDto uploadDto)
+        {
+            return new ObjectResult(uploadDto);
+        }
+    }
+}
